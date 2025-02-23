75dd533f652d70c11e3ffe7525571dcf74e39330
Daniel Bogan <d@waferbaby.com>
2025-02-23 20:45:00 +1100

    Add the search class method, override the screenshot_url and as_json methods

--- app/models/destiny_item.rb
+++ app/models/destiny_item.rb
@@ -1,2 +1,16 @@
 class DestinyItem < ApplicationRecord
+  def self.search(name)
+    where("lower(name) LIKE ?", "%" + sanitize_sql_like(name.downcase) + "%")
+  end
+
+  def screenshot_url
+    url = read_attribute(:screenshot_url)
+    return "" unless url.present?
+
+    "https://bungie.net#{url}"
+  end
+
+  def as_json(options = {})
+    super(options).except("id", "created_at", "updated_at")
+  end
 end
