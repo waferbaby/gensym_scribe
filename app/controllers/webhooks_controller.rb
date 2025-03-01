05b93dfff06d988858d327a1a68a76e400698f86
Daniel Bogan <d@waferbaby.com>
2025-03-02 10:52:22 +1100

    Start adding autocomplete support

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,17 +1,64 @@
 class WebhooksController < ApplicationController
-  wrap_parameters false
   before_action :validate_request
 
   def index
-    case params[:type]
-    when 0
+    case params[:type].to_i
+    when Discord::Interaction::PING
+      render json: { type: Discord::Interaction::PONG }
+    when Discord::Interaction::APPLICATION_COMMAND
+      process_command(params["data"]&.dig("options").first)
+    when Discord::Interaction::APPLICATION_COMMAND_AUTOCOMPLETE
+      process_autocomplete_command(params)
+    else
       head :no_content
-    when 1
-      render json: { type: 1 }
     end
   end
 
   private
+
+  def process_command(command)
+    raise "Unknown command" unless command.present?
+
+    case command.fetch(:name, false)
+    when "lore"
+        message = "Indeed."
+    when "screenshot"
+        message = "Nice."
+    end
+
+    response = {
+      type: 4,
+      data: {
+        tts: false,
+        content: message,
+        allowed_mentions: { parse: [] }
+      }
+    }
+
+    render json: response
+  end
+
+  def process_autocomplete_command(command)
+    query = command.dig(:data, :options)&.first.dig(:options).select { |option| option[:focused] == true }&.first.dig(:value)
+
+    items = DestinyItem.search(query)
+
+    choices =
+      if items.present?
+        items.map { |id, name| { name: name, value: id.to_s } }
+      else
+        []
+      end
+
+    response = {
+      type: 8,
+      data: {
+        choices: choices
+      }
+    }
+
+    render json: response
+  end
 
   def validate_request
     verification_key = Ed25519::VerifyKey.new([ ENV["DISCORD_APP_PUBLIC_KEY"] ].pack("H*"))
