150e86065e42ed8813dad6dbca331dd0047f4bca
Daniel Bogan <d@waferbaby.com>
2025-03-01 17:35:35 +1100

    Update to be under the module

---
+++ lib/discord/client.rb
@@ -0,0 +1,36 @@
+require "httpx"
+
+module Discord
+  class Client
+    def initialize(app_id, bot_id)
+      @app_id = app_id
+      @bot_id = bot_id
+    end
+
+    def register_commands(payload, guild_id = nil)
+      path =
+        if guild_id.present?
+          "/guilds/#{guild_id}/commands"
+        else
+          "/commands"
+        end
+
+      response = connection.post(path, json: payload)
+      response.raise_for_status
+
+      true
+    end
+
+    private
+
+    def connection
+      @connection ||= HTTPX.with(
+        origin: "https://discord.com",
+        base_path: "/api/v10/applications/#{@app_id}",
+        headers: {
+          authorization: "Bot #{@bot_id}"
+        }
+      )
+    end
+  end
+end
