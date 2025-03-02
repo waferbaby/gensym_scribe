4460cccdbd645174326d4370d443c08be90e9eef
Daniel Bogan <d@waferbaby.com>
2025-03-02 14:25:12 +1100

    Match lore entries and attach those to items

--- app/jobs/import_manifest_job.rb
+++ app/jobs/import_manifest_job.rb
@@ -2,25 +2,44 @@ class ImportManifestJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    manifest = Rails.cache.fetch("ManifestInventoryItems-#{Time.now.strftime('%Y-%m-%d')}") do
-      Restiny.download_manifest_json(definitions: [ Restiny::ManifestDefinition::INVENTORY_ITEM ])
+    definitions = {
+      items: Restiny::ManifestDefinition::INVENTORY_ITEM,
+      lore: Restiny::ManifestDefinition::LORE
+    }
+
+    manifest = Rails.cache.fetch("ZavalaClubManifest-#{Time.now.strftime('%Y-%m-%d')}") do
+      Restiny.download_manifest_json(definitions: definitions.values)
     end
 
     raise "Can't find items data" unless manifest.present?
 
-    path = manifest[Restiny::ManifestDefinition::INVENTORY_ITEM]
-    contents = File.read(path)
-    raise "Unable to read JSON" unless contents.present?
+    data = {}
 
-    JSON.parse(contents)&.each do |bungie_id, payload|
+    definitions.each do |name, definition|
+      contents = File.read(manifest[definition])
+      raise "Unable to read #{name} JSON" unless contents.present?
+
+      data[name] = JSON.parse(contents)
+    end
+
+    data[:items]&.each do |bungie_id, payload|
       item = {
-        bungie_id: bungie_id,
+        bungie_id: payload["hash"].to_s,
         name: payload.dig("displayProperties", "name"),
-        screenshot_url: payload["screenshot"]
+        screenshot_url: payload["screenshot"],
+        item_type: payload["itemType"],
+        item_sub_type: payload["itemSubType"],
+        class_type: payload["classTime"]
       }
+
+      if data[:lore].key?(item[:bungie_id])
+        item[:lore_entry] = data[:lore][item[:bungie_id]].dig("displayProperties", "description")
+      end
 
       DestinyItem.upsert(item, unique_by: :bungie_id)
     end
+
+    true
   rescue StandardError => e
     Rails.logger.error("Manifest import failed: #{e}")
   end
