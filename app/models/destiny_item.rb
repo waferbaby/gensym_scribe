c9c1d0879b46d373dc43e56a79718967a33abb3e
Daniel Bogan <d@waferbaby.com>
2025-03-02 14:24:30 +1100

    Add scopes for lore and screenshots

--- app/models/destiny_item.rb
+++ app/models/destiny_item.rb
@@ -1,10 +1,12 @@
 class DestinyItem < ApplicationRecord
+  scope :with_lore, -> { where.not(lore_entry: "") }
+  scope :with_screenshot, -> { where.not(screenshot_url: "") }
+
   def self.search(name, limit: 10)
     where("lower(name) LIKE ?", sanitize_sql_like(name.downcase) + "%")
       .where.not(name: "")
       .limit(limit)
       .order(name: :asc)
-      .pluck(:bungie_id, :name)
   end
 
   def screenshot_url
